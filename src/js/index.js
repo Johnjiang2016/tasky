1abc662c87c4f0415b9c5190d585d78c67d49e35
john.jiang <jiangchengbao@hotmail.com>
2023-06-24 15:47:48 +0800

    还原源代码

--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-const { ipcRenderer } = require('electron');
-
-const domClose = document.querySelector('.close');
-
-domClose.addEventListener('click', () => {
-    ipcRenderer.send('mainWin:close',{msg:'我是自定义消息！'})
-})
-
